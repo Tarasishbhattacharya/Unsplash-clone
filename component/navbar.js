942467a51a63cf986a56ce2a4a54965c03caeeb2
Tarasish bhattacharya <tarasishbhattacharya06@gmail.com>
2022-02-21 23:20:05 +0530

    new

---
+++ component/navbar.js
@@ -0,0 +1,72 @@
+function nav(){
+    return ` <div id="nav_container">
+    <div id="upper">
+        <div id="logo">
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAOVBMVEX///8AAAD39/eVlZVhYWHt7e2YmJisrKydnZ3AwMAuLi7V1dWoqKibm5v09PSQkJB1dXUdHR1aWlrVATWbAAABDklEQVR4nO3ZSxaCMAxAUUUFFfG3/8U6t5OAVVLPvQvIyeuw2WwAAAAAAAAAAAAAAELG/hjVj2svu8hlG3dZe9lFhhmFw9rLLqJQYX4KFeanUGF+ChXmp1BhfgoV5qdQYX4KFeanUGF+Ctsv/P/r2jiFL6RTmxdSAAAAIL3rIeC65sAPdbfI79JuxsRdZOCt+1rRuy60UD9jYh96MoX1KFRYUqiwNoUKSwoV1qZQYUmhwtoUKiwpVFibQoUlhQprU6iw1GbhNGPi1GTh/RR3b7LwcY57NFlYnUKFChUqVKhQoUKFChUqVKhQoUKFChUqVKhQoUKFLRYOz/3vPYffFQIAAAAAAAAAAAAAAPyxF6hYLRF9pu91AAAAAElFTkSuQmCC" alt="logo">
+        </div>
+        <div id="box_cont">
+            <div class="box">
+                <i class="material-icons">search</i>
+                    <input type="text" name="" id="" placeholder="search free-resolution photos">
+                    <i class="material-icons">qr_code_scanner</i> 
+            </div>
+        </div>
+        <div id="list">
+            <div>
+                <a href="">Explore</a>
+                <a href="">Advertise</a>
+                <a href="">Blog</a>
+            </div>
+        </div>
+        <div id="upper_right"></div>
+        <div id="right_cont">
+        <div id="right">
+            <a href="login.html">Log in</a>
+            <button id="photo_btn">submit a photo</button>
+            <i class="material-icons">menu</i>
+        </div>
+    </div>
+    </div>
+    <div id="lower">
+        <div id="editorial">Editorial</div>
+        <div id="lower_right"></div>
+        <div id="lower_list">
+            <a href="/entrpreneur.html"  class="border">
+                <span>Featured <br><span id="entrpreneur">Entrepreneur</span></span>
+            </a>
+            <a href="" class="border">
+                <span>3D Renders</span>
+            </a>
+            <a href="" class="border">
+                <span>Textures & patterns</span>
+            </a>
+            <a href="" class="border">
+                <span>Architecture</span>
+            </a>
+            <a href="" class="border">
+                <span>Experimental</span>
+            </a>
+            <a href="" class="border">
+                <span>Nature</span>
+            </a>
+            <a href="" class="border">
+                <span>Business & work</span>
+            </a>
+            <a href="" class="border">
+                <span>Fashion</span>
+            </a>
+            <a href="" class="border">
+                <span>Film</span>
+            </a>
+            <a href="" class="border">
+                <span>Food & Drink</span>
+            </a>
+            <a href="" class="border">
+                <span>Health & wellness</span>
+            </a>
+            
+        </div>
+    </div>
+</div>`
+}
+export default nav;
